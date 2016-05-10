bb3031daa98dab177511da960da2a2e783527018
Fractals- <fractals.me@gmail.com>
2016-05-10 10:05:09 +0200

    mouse jobs

--- general.h
+++ general.h
@@ -9,7 +9,7 @@
 #include <cstdlib>
 #include <float.h>
 
-const int max_n_elements = 10000000;//770000000;
-const int max_n_rows = 200000; //27993600;
+const int max_n_elements = 35000000;//770000000;
+const int max_n_rows = 500000; //27993600;
 
 #endif /* __GENERAL_H__ */
