95efa079d5cf79af83bc0aa3544762bb5b8b5559
Fractals- <fractals.me@gmail.com>
2016-05-16 15:45:02 +0200

    gen2

--- general.h
+++ general.h
@@ -9,7 +9,7 @@
 #include <cstdlib>
 #include <float.h>
 
-const int max_n_elements = 50000000;//774472352;
-const int max_n_rows = 27993600;//1000000;
+const int max_n_elements = 774472352;//50000000;
+const int max_n_rows = 1000000;//27993600;//
 
 #endif /* __GENERAL_H__ */
